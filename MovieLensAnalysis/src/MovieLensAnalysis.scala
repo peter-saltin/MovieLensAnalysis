e4bd724dfcdc32e2729d15672e4f1b6540b01f12
peter-saltin <peter.saltin@gmail.com>
2015-07-25 20:01:10 +0200

    settings

--- MovieLensAnalysis/src/MovieLensAnalysis.scala
+++ MovieLensAnalysis/src/MovieLensAnalysis.scala
@@ -12,11 +12,9 @@ object MovieLensAnalysis {
     val conf = new SparkConf().setAppName("Movielens Analysis")
     val sc = new SparkContext(conf)
     val hadoopConf=sc.hadoopConfiguration
-      .set("fs.s3.impl", "org.apache.hadoop.fs.s3native.NativeS3FileSystem")
-      .set("fs.s3n.awsAccessKeyId",sys.env("AWS_ACCESS_KEY_ID"))
-      .set("fs.s3n.awsSecretAccessKey",sys.env("AWS_SECRET_ACCESS_KEY"));
-    
-
+    hadoopConf.set("fs.s3.impl", "org.apache.hadoop.fs.s3native.NativeS3FileSystem")
+    hadoopConf.set("fs.s3n.awsAccessKeyId",sys.env("AWS_ACCESS_KEY_ID"))
+    hadoopConf.set("fs.s3n.awsSecretAccessKey",sys.env("AWS_SECRET_ACCESS_KEY"))
 
     val pathFile = "s3n://saltin1/input/ampcamp/movielens/large/ratings.dat"
     val dataFile = sc.textFile(pathFile, 2).cache()
